51271639b73f86c6f80c4fc97376106b701972c0
shahoxo <mottipok@hotmail.com>
2012-10-30 13:16:09 +0900

    add controller methods

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,10 +1,48 @@
 class PhrasesController < ApplicationController
+  before_filter :find_munoh
+
   def index
+    @phrases = @munoh.phrases
   end
 
   def new
+    @phrase = @munoh.phrases.new
   end
 
   def edit
+    @phrase = Phrase.find(params[:id])
+  end
+
+  def create
+    @phrase = @munoh.phrases.new(params[:phrase])
+    if @phrase.save
+      redirect_to munoh_phrases_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @phrase = @munoh.phrases.find(params[:id])
+    if @phrase.update_attributes(params[:phrase])
+      redirect_to munoh_phrases_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    phrase = @munoh.phrases.find(params[:id])
+    phrase.destroy
+    redirect_to munoh_phrases_url
+  end
+
+  private
+  def find_munoh
+    @munoh = Munoh.find_by_id(params[:munoh_id])
+    unless @munoh
+      redirect_to munohs_url
+      return false
+    end
   end
 end
