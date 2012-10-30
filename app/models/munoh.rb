1c1cc3e79aa28265c5993431977ec017ccad5696
shahoxo <mottipok@hotmail.com>
2012-10-30 13:14:02 +0900

    fix nested resources

--- app/models/munoh.rb
+++ app/models/munoh.rb
@@ -1,5 +1,5 @@
 class Munoh < ActiveRecord::Base
-  attr_accessible :name, :twitter_name 
+  attr_accessible :name, :twitter_name, :phrases_attributes
 
   has_many :rooms
   has_many :phrases
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 ChatMunoh::Application.routes.draw do
-  get "phrases/index"
+  resources :munohs do
+    resources :phrases
+  end
 
-  get "phrases/new"
-
-  get "phrases/edit"
-
-  resources :munohs
   resources :rooms do
     resources :talks
   end
